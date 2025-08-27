6c76e267d9adbf43473eca0a6b5e7193255a5900
rivelino86 <157528687+rivelino86@users.noreply.github.com>
2025-08-26 21:49:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,17 +22,3 @@ sudo wget -O dev.zip https://github.com/kserge2001/web-consulting/archive/refs/h
 sudo unzip -o dev.zip
 sudo cp -r /opt/web-consulting-dev/* /var/www/html/
 sudo chown -R apache:apache /var/www/html
-
-
-
-# #!/bin/bash
-# sudo  yum update -y
-# sudo   groupadd docker
-# sudo   useradd John -aG docker 
-# sudo   yum install git unzip wget httpd -y
-# sudo   systemctl start httpd
-# sudo   systemctl enable httpd
-#        cd /opt
-# sudo   wget https://github.com/kserge2001/web-consulting/archive/refs/heads/dev.zip
-# sudo   unzip dev.zip
-# sudo   cp -r /opt/web-consulting-dev/* /var/www/html
